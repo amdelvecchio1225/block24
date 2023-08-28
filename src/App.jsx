242edba48ce7e6406d68184a59edd523a5916980
Anthony DelVecchio <amdelvecchio1225@gmail.com>
2023-08-28 14:07:36 -0400

    section 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import {puppyList} from './data.js'
+import {useState} from 'react'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
     <>
-      <div>
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
